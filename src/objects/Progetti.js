9ab2ca2f62b407860d78cd8899008307994e7738
Reveryyy <reveryycontact@gmail.com>
2026-08-24 13:20:26 +0200

    Deleted more project test

--- src/objects/Progetti.js
+++ src/objects/Progetti.js
@@ -12,19 +12,6 @@ const progetti = [
     stato_it: "Completato",
     stato_en: "Completed",
   },
-  {
-    nome_it: "Portfolio",
-    nome_en: "Portfolio",
-    description_it:
-      "Questa pagina che stai vedendo. Il mio portfolio personale contenente tutte le informazioni necessarie e i miei progetti.",
-    description_en:
-      "This page you're viewing is my personal portfolio, containing all the information you need about me and my projects.",
-    anno: "2026",
-    tags: ["React", "HTML/CSS"],
-    github: "https://github.com/Reveryyy/portfolio",
-    stato_it: "Completato",
-    stato_en: "Completed",
-  },
 ];
 
 export default progetti;
